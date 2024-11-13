3df9dc0cde3c93ad1921ff344979bde66c233026
Wayne O. Cochran <wayne.cochran@gmail.com>
2024-11-13 15:37:32 -0800

    added Phoenix NASCAR frames

--- stereo-test/stereo-pair.sh
+++ stereo-test/stereo-pair.sh
@@ -4,8 +4,12 @@
 #R="HAI2-rectified.jpg-lines.jpg"
 #L="braves-rectified-left.jpg"
 #R="braves-rectified-right.jpg"
-L="summer-league-left.jpg"
-R="summer-league-right.jpg"
-OUT="summer-league.mp4"
+#L="summer-league-left.jpg"
+#R="summer-league-right.jpg"
+#L="phx1-left-rectified-lines.jpg"
+#R="phx1-right-rectified-lines.jpg"
+L="phx2-left-rectified-lines.jpg"
+R="phx2-right-rectified-lines.jpg"
+OUT="phx2.mp4"
 
 ffmpeg -i $L -i $R -filter_complex hstack=inputs=2 -pix_fmt yuv420p $OUT
