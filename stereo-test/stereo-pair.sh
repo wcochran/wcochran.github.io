3c5148a49e614474679f8e04ea8e280cc3e7b68d
Wayne Cochran <waynec@quintar.ai>
2024-12-18 20:50:12 -0800

    Updated staereo-pair script.

--- stereo-test/stereo-pair.sh
+++ stereo-test/stereo-pair.sh
@@ -4,8 +4,11 @@
 #R="HAI2-rectified.jpg-lines.jpg"
 #L="braves-rectified-left.jpg"
 #R="braves-rectified-right.jpg"
-L="summer-league-left.jpg"
-R="summer-league-right.jpg"
-OUT="summer-league.mp4"
+#L="summer-league-left.jpg"
+#R="summer-league-right.jpg"
+L="phoenix-left.jpg"
+R="phoenix-right.jpg"
+#OUT="summer-league.mp4"
+OUT="phoenix.mp4"
 
 ffmpeg -i $L -i $R -filter_complex hstack=inputs=2 -pix_fmt yuv420p $OUT
